25c438db0a2203f14381799f0fb522adecf6f079
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-03-22 10:16:47 +0000

    Force MacOS X to use OpenGL 3.2; thats the maximum it currently supports.

--- example/oglplus/glfw_main.cpp
+++ example/oglplus/glfw_main.cpp
@@ -149,7 +149,11 @@ void run_example(
 	const GLuint width = 800, height = 600;
 
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+#ifdef __APPLE__
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+#else
+	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+#endif
 	glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	if(!glfwOpenWindow(
