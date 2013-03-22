6c26f42011694a7e0ee62d7a95f7abab65086cc1
Tai Chi Minh Ralph Eastwood <tcmreastwood@gmail.com>
2013-03-22 07:17:07 +0000

    Fix building with non-X11 OpenGL on MacOS X.

--- config/FindOpenGL.cmake
+++ config/FindOpenGL.cmake
@@ -2,6 +2,7 @@
 #  Software License, Version 1.0. (See accompanying file
 #  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
+include(FindOpenGL)
 unset(OPENGL_INCLUDE_DIRS)
 set(OPENGL_FOUND false)
 #
@@ -14,6 +15,11 @@ find_path(
 # if that didn't work try the system directories
 if((NOT OPENGL_GL3_H_DIR) OR (NOT EXISTS ${OPENGL_GL3_H_DIR}))
 	find_path(OPENGL_GL3_H_DIR GL3/gl3.h)
+endif()
+if(NOT EXISTS ${OPENGL_GL3_H_DIR})
+	find_path(OPENGL_GL3_H_DIR gl3.h
+		PATHS /System/Library/Frameworks/OpenGL.framework/Versions/A/Headers
+	)
 endif()
 # if found append it to the include directories
 if((OPENGL_GL3_H_DIR) AND (EXISTS ${OPENGL_GL3_H_DIR}))
@@ -36,19 +42,4 @@ endif()
 if((OPENGL_GLCOREARB_H_DIR) AND (EXISTS ${OPENGL_GLCOREARB_H_DIR}))
 	set(OPENGL_INCLUDE_DIRS ${OPENGL_INCLUDE_DIRS} ${OPENGL_GLCOREARB_H_DIR})
 	set(OPENGL_FOUND true)
-endif()
-
-# try to find the GL library
-find_library(
-	OPENGL_LIBRARIES NAMES GL OpenGL32
-	PATHS ${LIBRARY_SEARCH_PATHS}
-	NO_DEFAULT_PATH
-)
-if(NOT OPENGL_LIBRARIES)
-	find_library(OPENGL_LIBRARIES NAMES GL OpenGL32)
-endif()
-
-#if we have not found the library
-if(NOT OPENGL_LIBRARIES)
-	set(OPENGL_LIBRARIES "")
 endif()
